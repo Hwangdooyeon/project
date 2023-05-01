5bb8a86faa42c069ccab81a3cb2e1768e31cc99a
Hwang doo yeon <109845059+Hwangdooyeon@users.noreply.github.com>
2023-05-02 01:12:14 +0900

    - Member기능 추가

--- src/main/java/com/example/demo/member/controller/MemberController.java
+++ src/main/java/com/example/demo/member/controller/MemberController.java
@@ -1,20 +1,42 @@
 package com.example.demo.member.controller;
 
+import com.example.demo.member.dto.MemberDto;
+import com.example.demo.member.entity.Member;
+import com.example.demo.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import java.lang.reflect.Member;
+import java.util.List;
 
 @Controller
 public class MemberController {
-//private final Memberservice memberservice;
-
-  @GetMapping("/member/new")
-    private String createForm(){
-      return "member/createMemberForm";
+  private final MemberService memberservice;
+   @Autowired
+  public MemberController(MemberService memberservice) {
+    this.memberservice = memberservice;
   }
 
+  @GetMapping("/member/new")
+    private String createForm() {
+      return "member/createMemberForm";
+
+}
+@PostMapping("/member/new")
+public String create(MemberDto form){
+     Member member = new Member();
+     member.setName(form.getName());
+     member.setEmail(form.getEmail());
+     memberservice.join(member);
+
+     return "redirect";
+   }
+   @GetMapping("/member/list")
+  public String list(Model model){
+     List<Member> members = memberservice.findMembers();
+     model.addAttribute("members",members);
+     return "member/memberList";
+   }
 }
